70ea226bc58179e1d532a01bb1fb2b1168f98264
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-09-12 02:30:17 +0100

    Added a class Square that inherits from Rectangle (9-rectangle.py) file

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,11 +3,13 @@
 Defines the class BaseGeometry and subclass Rectangle
 """
 
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """A representation of a square"""
+
     def __init__(self, size):
         """instantiation of the square"""
         self.integer_validator("size", size)
