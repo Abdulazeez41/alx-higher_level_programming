42c6575ea36052f6b9bfdf0b72708cc1b4fe0e51
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-09-12 02:24:22 +0100

    Added a class BaseGeometry (based on 7-base_geometry.py) file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -10,19 +10,22 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     '''
         Implements a rectangle
+        Private instance attributes:
+        - width
+        - height
+    Inherits from BaseGeometry
     '''
 
     def __init__(self, width, height):
-    '''
-        Constructor to assign width and height at runtime
+        '''
+            Constructor to assign width and height at runtime
 
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
-    '''
+            Args:
+                width (int): The width of the new Rectangle.
+                height (int): The height of the new Rectangle.
+        '''
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
