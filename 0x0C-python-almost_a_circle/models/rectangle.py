a11d1a3833dd25453905494294e92c9825319aeb
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2023-09-19 01:01:40 +0100

    Added setters/getters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,47 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """Get private attrb"""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get private attrb"""
+        return self.__height
+
+    @property
+    def x(self):
+        """Get private attrb"""
+        return self.__x
+
+    @property
+    def y(self):
+        """Get private attrb"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        """Set private attrb"""
+        self.setter_validation("width", value)
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """Set private attrb"""
+        self.setter_validation("height", value)
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        """Set private attrb"""
+        self.setter_validation("x", value)
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        """Set private attrb"""
+        self.setter_validation("y", value)
+        self.__y = value
